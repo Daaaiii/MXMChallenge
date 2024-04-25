564acc77bd961bca17f1830307d864c053171274
Daiane Bolzan <dainha85@gmail.com>
2024-04-25 12:29:28 -0300

    feat: add routes to home, register and register success

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./components/register/register.component').then(
+        (m) => m.RegisterComponent
+      ),
+  },
+  {
+    path: 'register-success',
+    loadComponent: () =>
+      import('./components/register-success/register-success.component').then(
+        (m) => m.RegisterSuccessComponent
+      ),
+  },
+];
