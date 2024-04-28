727d5f33d0ce6473015719e5b2dec1bfff49e962
Daiane Bolzan <dainha85@gmail.com>
2024-04-28 13:59:18 -0300

    feat: add profile page

---
+++ src/app/components/profile/profile.component.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { RegisterDTO } from '../../models/registerDTO';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [NavbarComponent, RouterLink, ReactiveFormsModule],
+
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent {
+ 
+  
+  constructor(private authService: AuthService, private router:Router) {}
+
+
+  page= 'Logout';
+  route= '/home';
+  username:string = localStorage.getItem('User') || '';
+  user: RegisterDTO = {} as RegisterDTO;
+  ngOnInit() {
+    this.getUserInfo();
+  }
+  onLogout() {
+    this.authService.logout();
+  }
+
+  
+  deleteProfile() {
+    if (this.authService.isLoggedIn()) { 
+    this.authService.deleteProfile().subscribe({
+      next: () => {
+        alert('Profile deleted');
+        this.router.navigate(['/home']);
+      },
+      error: (err) => console.error('Deletion failed', err)
+    })} else {
+      console.error('Authentication token missing');
+    }
+  }
+
+  getUserInfo() {
+    if (this.authService.isLoggedIn()) {  
+      this.authService.getProfile() 
+        .subscribe({
+          next: (data) => this.user = data,
+          error: (err) => console.error('Failed to get user data', err)
+        });
+    } else {
+      console.error('Authentication token missing');
+    }
+  }
+
+  updateProfile() {
+    this.router.navigate(['/update']);
+  }
+}
