43edcedea37870d1ec2ea5126b3e7a74244b915f
Daiane Bolzan <dainha85@gmail.com>
2024-04-28 13:58:24 -0300

    feat: add guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AuthGuard } from './guards/auth-guard.guard';
+
+
 
 
 export const routes: Routes = [
@@ -18,5 +21,31 @@ export const routes: Routes = [
       import('./components/register-success/register-success.component').then(
         (m) => m.RegisterSuccessComponent
       ),
+  },
+ 
+  {
+    path: '',
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'profile',
+        loadComponent: () =>
+          import('./components/profile/profile.component').then(
+            (m) => m.ProfileComponent
+          ),
+      }
+      
+    ],
+  },
+  {
+    path: 'not-found',
+    loadComponent: () =>
+      import('./components/not-found/not-found.component').then(
+        (m) => m.NotFoundComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found',
   },
 ];
